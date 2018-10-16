89f70d54bf4c9b6b043a77c2a3317d72651a7e03
RamZ <saikrn112@gmail.com>
2018-10-16 22:53:15 +0530

    updated new file aliases

--- 1 - Set Up Basic Environment.sh
+++ 1 - Set Up Basic Environment.sh
@@ -30,7 +30,8 @@ git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 cp ./config_files/zsh_config ~/.zim/templates/zimrc
 ln -s ~/.zim/templates/zimrc ~/.zimrc
 ln -s ~/.zim/templates/zlogin ~/.zlogin
-ln -s ~/.zim/templates/zshrc ~/.zshrc
+ln -sf $0/Ubuntu-install-scripts ~/Ubuntu-install-scripts 
+ln -sf ~/Ubuntu-install-scripts/config_files/zshrc ~/.zshrc 
 
 # If you prefer Prezto, then uncomment the next few lines. Zim is much faster though
 # git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
---
+++ config_files/aliases.sh
@@ -0,0 +1,57 @@
+currdir=$0
+
+# ROS Aliases 
+alias sRos='source /opt/ros/kinetic/setup.zsh'
+alias sR='source ./devel/setup.zsh'
+alias sZ='source /media/Extra/flight_stack/zed_ws/devel/setup.zsh'
+alias rosIP= 'export ROS_IP=$(hostname -I)'
+alias rosMultitX2='export ROS_IP='10.42.0.115' && export ROS_MASTER_URI=http://10.  42.0.1:11311'
+alias rosAbhi='export ROS_IP=192.168.0.4 && export ROS_MASTER_URI=http://192.168.0. 3:11311'
+
+# Anacoda Stuff
+#export PATH="/home/bhagyasyam/anaconda3/bin:$PATH"
+
+# Productivity Hacks
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+alias ..5="cd ../../../../.."
+alias sZsh="source ~/.zshrc"
+
+# Useful Functions
+function bk () { bksuffix=`date +%Y%m%d%H%M%S`; newname=$1.$bksuffix; cp $1 $newname; echo "$1 --> $newname"}
+function mkdircd () { mkdir -p "$@" && eval cd "\"\$$#\""; }
+
+CDPATH=/media/Extra
+# PX4 stuff
+export PATH=/media/bhagyasyam/Extra/gcc-arm-none-eabi-7-2017-q4-major/bin:$PATH
+
+# Git Aliases
+alias gs="git status"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gac="git commit -am "
+alias gp="git push"
+alias gpum="git pull origin master"
+alias gpom="git push origin master"
+alias gpot="git push origin --tags"
+alias gitSS="/media/Extra/flight_stack/git-submodule-selective.sh"
+
+# Gen aliases
+alias sad="sudo apt update"
+alias sag="sudo apt dist-upgrade"
+alias sai="sudo apt install"
+alias sar="sudo apt remove"
+alias dur="du -a -h -d 1 | sort -h -r"
+alias dirSize="dur"
+alias lt="ls -alrth"
+
+# Mounting remote file system
+alias mntTX2='sudo sshfs -o allow_other nvidia@10.21.123.56:/ /mnt/nvidiA'
+
+# Docker stuff 
+alias di='docker images -a'
+alias dps='docker ps -a -s'
+alias dsys='docker system df'
+
